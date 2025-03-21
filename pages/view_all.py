72359ea2c6a24468f537c7d9e9785f84860ebd53
Qwintal <144579285+Qwintal@users.noreply.github.com>
2025-03-21 10:11:17 +0530

    Update view_all.py

--- pages/view_all.py
+++ pages/view_all.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.logo("C:/Users/Mrank/Python/Sprits/pokeball.png",)
+st.logo("Sprits/pokeball.png",)
 df = pd.read_csv("pokemon.csv")
 st.title("All Pokemon in Database \n (till Gen 7)")
 selected_columns = ["pokedex_number","name","type1","type2","base_total","classfication","height_m","weight_kg","percentage_male","generation","is_legendary"]
